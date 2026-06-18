48ee4be45d75f491c564bb7289bf174d552845ed
hugo-gaudre <hugo.gaudre@icloud.com>
2026-06-18 09:15:20 +0200

    Remove token creation from the admin interface

--- auth_management/apps.py
+++ auth_management/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class AuthManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auth_management'
+
+    def ready(self):
+        from django.contrib import admin
+        from rest_framework.authtoken.models import Token
+
+        try:
+            admin.site.unregister(Token)
+        except admin.sites.NotRegistered:
+            pass
